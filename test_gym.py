5b1d03d57501a9bda55483651061d7fe4190a80a
quessy <aq15777@bristol.ac.uk>
2022-08-01 21:51:49 +0100

    added wrappers and test

---
+++ test_gym.py
@@ -0,0 +1,31 @@
+import gym
+from wrappers.gym_wrapper import GymWrapper
+from url_benchmark import dmc
+
+def main():
+    env = dmc.make('CarRacing-v2',
+                    obs_type='states',
+                    frame_stack=3,
+                    action_repeat=1,
+                    seed=42)
+    print(f'action_spec is: {env.action_spec()}')
+    time_step = env.reset()
+    last_episode = False
+    ide = 0
+    while not last_episode:
+        ide += 1
+        # print(f'time_step: {time_step}')
+        print(f'observation: {time_step.observation.shape}')
+        action = env.action_space.sample()
+        # print(f'action: {action}')
+        time_step = env.step(action)
+        if time_step.last():
+            last_episode = True
+            print(f'time_step: {time_step}')
+            print(f'iterated through: {ide}')
+            time_step = env.reset()
+    env.close()
+
+
+if __name__=='__main__':
+    main()
