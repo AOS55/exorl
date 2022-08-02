7fc9d2df1e563ee4276440509091ca7754519335
quessy <aq15777@bristol.ac.uk>
2022-08-02 15:11:16 +0100

    constructor change

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-from .env_constructor import make
+from .env_constructor import make, ENV_TYPES
 from .logger import AverageMeter, MetersGroup, Logger, LogAndDumpCtx
 from .replay_buffer import episode_len, save_episode, load_episode, relable_episode, OfflineReplayBuffer, ReplayBufferStorage, ReplayBuffer, make_offline_replay_loader, make_replay_loader
 from .utils import eval_mode, set_seed_everywhere, chain, soft_update_params, hard_update_params, to_torch, weight_init, grad_norm, param_norm, Until, Every, Timer, TruncatedNormal, TanhTransform, SquashedNormal, schedule, RandomShiftsAug, RMS, PBE
