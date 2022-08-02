33a66bd20d639982423bab26e2f60c324ba1e967
quessy <aq15777@bristol.ac.uk>
2022-08-02 15:11:06 +0100

    changed make func

--- test_gym.py
+++ test_gym.py
@@ -1,22 +1,23 @@
 import gym
-from utils import GymWrapper
-import utils.env_constructor as constructor
+from utils.wrappers import GymWrapper
+from utils.env_constructor import make
 
 
 def main():
-    env = constructor.make('BipedalWalker-v3',
-                           obs_type='states',
-                           frame_stack=3,
-                           action_repeat=1,
-                           seed=42)
+    env = make('BipedalWalker-v3',
+                obs_type='states',
+                frame_stack=3,
+                action_repeat=1,
+                seed=42)
     print(f'action_spec is: {env.action_spec()}')
     time_step = env.reset()
+    print(f'render: {env.render()}')
     last_episode = False
     ide = 0
     while not last_episode:
         ide += 1
         # print(f'time_step: {time_step}')
-        print(f'observation: {time_step.observation.shape}')
+        # print(f'observation: {time_step.observation.shape}')
         action = env.action_space.sample()
         # print(f'action: {action}')
         time_step = env.step(action)
