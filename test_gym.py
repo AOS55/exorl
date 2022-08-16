2339d06b809b0a568dbef47e46edea771dc2b987
quessy <aq15777@bristol.ac.uk>
2022-08-16 13:55:55 +0100

    commented out minor sections

--- test_gym.py
+++ test_gym.py
@@ -4,7 +4,7 @@ from utils.env_constructor import make
 
 
 def main():
-    env = make('BipedalWalker-v3',
+    env = make('SimplePointBot',
                 obs_type='states',
                 frame_stack=3,
                 action_repeat=1,
@@ -21,11 +21,16 @@ def main():
         action = env.action_space.sample()
         # print(f'action: {action}')
         time_step = env.step(action)
+        print(env._env._env._env._env.get_info())
         if time_step.last():
             last_episode = True
-            print(f'time_step: {time_step}')
-            print(f'iterated through: {ide}')
+            # print(f'time_step: {time_step}')
+            # print(f'iterated through: {ide}')
             time_step = env.reset()
+
+            print(f'dir(time_step): {dir(time_step)}')
+            print(env._env._env._env._env.get_info())
+            # print(f'info is: {time_step.get_info()}')
     env.close()
 
 
