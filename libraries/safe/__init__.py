408732a36f1b8ffedc1c9ac36111ecc2906e4393
quessy <aq15777@bristol.ac.uk>
2022-08-24 15:42:26 +0100

    correct BottleNeck name

--- libraries/safe/__init__.py
+++ libraries/safe/__init__.py
@@ -1,3 +1,3 @@
 from .simple_point_bot import SimplePointBot, SimplePointBotLong
 from .simple_velocity_bot import SimpleVelocityBot
-from .bottleneck_nav import Bottleneck
+from .bottleneck_nav import BottleNeck
