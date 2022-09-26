6430861bdd47b4710f2a93b3d8666445acf1fcbf
quessy <aq15777@bristol.ac.uk>
2022-09-26 14:42:22 +0100

    added function to download all models on s3

--- download_data.py
+++ download_data.py
@@ -1,4 +1,3 @@
-import subprocess
 import os
 import boto3
 from botocore import UNSIGNED
@@ -9,13 +8,13 @@ import numpy as np
 import torch
 
 
-def read_data_from_s3(env_type='states', env='SimplePointBot', agent='diayn', priors=10, pretrain_steps=50000, root_dir='data/datasets'):
+def download_data_from_s3(obs_type='states', env='SimplePointBot', agent='diayn', priors=10, pretrain_steps=50000, root_dir='data/datasets'):
 
     if agent=='diayn':
-        elems = [env_type, env, agent, str(priors), str(pretrain_steps)]
+        elems = [obs_type, env, agent, str(priors), str(pretrain_steps)]
         dir_path = os.path.join(*elems)
     else:
-        elems = [env_type, env, agent, str(pretrain_steps)]
+        elems = [obs_type, env, agent, str(pretrain_steps)]
         dir_path = os.path.join(*elems)
 
     bucket_name = 'urlsuite-data'
@@ -44,13 +43,13 @@ def read_data_from_s3(env_type='states', env='SimplePointBot', agent='diayn', pr
     return None
 
 
-def read_models_from_s3(env_type='states', env='SimplePointBot', agent='diayn', priors=20, seed=1, root_dir='data/models'):
+def download_model_from_s3(obs_type='states', env='SimplePointBot', agent='diayn', priors=20, seed=1, root_dir='data/models'):
 
     if agent=='diayn':
-        elems = [env_type, env, agent, str(priors), str(seed)]
+        elems = [obs_type, env, agent, str(priors), str(seed)]
         dir_path = os.path.join(*elems)
     else:
-        elems = [env_type, env, agent, seed]
+        elems = [obs_type, env, agent, seed]
         dir_path = os.path.join(*elems)
 
     bucket_name = 'urlsuite-models'
@@ -72,10 +71,24 @@ def read_models_from_s3(env_type='states', env='SimplePointBot', agent='diayn', 
             url = f'https://{bucket_name}.{resource_type}.{region_name}.amazonaws.com/{key}'
             r = http.request('GET', url)
             model = torch.load(BytesIO(r.data))
-            print(f'key: {key}')
-            print(f'data: {dir_path}')
+            torch.save(model, os.path.join(root_dir, key))
+
+def download_all_models_from_s3(root_dir='data/models'):
+    bucket_name = 'urlsuite-models'
+    region_name = 'eu-west-2'
+    resource_type = 's3'
+    s3 = boto3.client(resource_type, region_name=region_name, config=Config(signature_version=UNSIGNED))
+    response = s3.list_objects_v2(Bucket=bucket_name, Prefix='')
+    files = response.get("Contents")
+    http = urllib3.PoolManager()
+    os.mkdir(root_dir)
+    for file in files:
+            key = file['Key']
+            url = f'https://{bucket_name}.{resource_type}.{region_name}.amazonaws.com/{key}'
+            r = http.request('GET', url)
+            model = torch.load(BytesIO(r.data))
             torch.save(model, os.path.join(root_dir, key))
 
 
 if __name__=='__main__':
-    read_models_from_s3()
+    download_model_from_s3()
