3168fadcb0764924914c90533d3a61e9d110f5c5
quessy <aq15777@bristol.ac.uk>
2022-09-26 14:12:53 +0100

    added method to write models from model bucket

--- download_data.py
+++ download_data.py
@@ -6,6 +6,7 @@ from botocore.client import Config
 import urllib3
 from io import BytesIO
 import numpy as np
+import torch
 
 
 def read_data_from_s3(env_type='states', env='SimplePointBot', agent='diayn', priors=10, pretrain_steps=50000, root_dir='data/datasets'):
@@ -21,8 +22,6 @@ def read_data_from_s3(env_type='states', env='SimplePointBot', agent='diayn', pr
     region_name = 'eu-west-2'
     resource_type = 's3'
     s3 = boto3.client(resource_type, region_name=region_name, config=Config(signature_version=UNSIGNED))
-    # resource = boto3.resource('s3', region_name='eu-west-2', config=Config(signature_version=UNSIGNED))
-    # core_bucket = resource.Bucket(bucket_name)
     write_dir = os.path.join(root_dir, dir_path)
 
     if os.path.exists(write_dir):
@@ -45,5 +44,38 @@ def read_data_from_s3(env_type='states', env='SimplePointBot', agent='diayn', pr
     return None
 
 
+def read_models_from_s3(env_type='states', env='SimplePointBot', agent='diayn', priors=20, seed=1, root_dir='data/models'):
+
+    if agent=='diayn':
+        elems = [env_type, env, agent, str(priors), str(seed)]
+        dir_path = os.path.join(*elems)
+    else:
+        elems = [env_type, env, agent, seed]
+        dir_path = os.path.join(*elems)
+
+    bucket_name = 'urlsuite-models'
+    region_name = 'eu-west-2'
+    resource_type = 's3'
+    s3 = boto3.client(resource_type, region_name=region_name, config=Config(signature_version=UNSIGNED))
+    write_dir = os.path.join(root_dir, dir_path)
+
+    if os.path.exists(write_dir):
+        print(f'file path already exists: {write_dir}')
+        return None
+    else:
+        response = s3.list_objects_v2(Bucket=bucket_name, Prefix=dir_path)
+        files = response.get("Contents")
+        http = urllib3.PoolManager()
+        os.mkdir(write_dir)
+        for file in files:
+            key = file['Key']
+            url = f'https://{bucket_name}.{resource_type}.{region_name}.amazonaws.com/{key}'
+            r = http.request('GET', url)
+            model = torch.load(BytesIO(r.data))
+            print(f'key: {key}')
+            print(f'data: {dir_path}')
+            torch.save(model, os.path.join(root_dir, key))
+
+
 if __name__=='__main__':
-    read_data_from_s3()
+    read_models_from_s3()
