5bf7d0b1b62003aed0a156049bc57f3aaa67a60d
quessy <aq15777@bristol.ac.uk>
2022-10-25 10:52:21 +0100

    sampling_batch updated to use smm and prioritized_sampling

--- sampling_batch.sh
+++ sampling_batch.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo ${1}
-for AGENT in diayn
+echo seed ${1}
+echo skill dim ${2}
+for AGENT in smm
     do for SNAPSHOT_TS in 10000 50000 100000 500000 1000000 1500000 2000000
-        do python sampling.py seed=${1} skill_dim=${2} agent=$AGENT obs_type=states task=SimplePointBot_goal data_type=unsupervised snapshot_ts=$SNAPSHOT_TS
+        do python prioritized_sampling.py seed=${1} skill_dim=${2} agent=$AGENT obs_type=states task=SimplePointBot_goal data_type=unsupervised snapshot_ts=$SNAPSHOT_TS
     done
 done
