9702241f5916f17519f1e599fa4e9c7f4351ccfc
quessy <aq15777@bristol.ac.uk>
2022-12-01 12:36:41 +0000

    change to skill dim and snapshots

--- sampling_batch.sh
+++ sampling_batch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo seed ${1}
-for SKILL_DIM in 20
+for SKILL_DIM in 50
     do for AGENT in smm
-        do for SNAPSHOT_TS in 7000000 8000000 9000000 10000000 11000000 12000000 13000000 14000000 15000000 16000000
+        do for SNAPSHOT_TS in 10000 50000 100000 500000 1000000 1500000 2000000 3000000 4000000 5000000 6000000 7000000 8000000 9000000 10000000 11000000 12000000 13000000 14000000 15000000 16000000
             do python prioritized_sampling.py seed=${1} skill_dim=$SKILL_DIM agent=$AGENT obs_type=states task=SimplePointBot_goal data_type=unsupervised snapshot_ts=$SNAPSHOT_TS
         done
     done
