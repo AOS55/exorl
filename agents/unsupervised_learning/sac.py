f04d06038526a7f4a736eeaf18e55e558b935a76
quessy <aq15777@bristol.ac.uk>
2023-01-09 12:58:41 +0000

    added sac implementation with different replay buffer

---
+++ agents/unsupervised_learning/sac.py
@@ -0,0 +1,234 @@
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+from torch.distributions import Normal, Independent
+
+from utils.replay_buffer import Batch
+import utils
+
+def get_net(
+        num_in:int,
+        num_out:int,
+        final_activation,  # e.g. nn.Tanh
+        num_hidden_layers:int=5,
+        num_neurons_per_hidden_layer:int=64
+    ) -> nn.Sequential:
+
+    layers = []
+    layers.extend([
+        nn.Linear(num_in, num_neurons_per_hidden_layer),
+        nn.ReLU(),
+    ])
+    for _ in range(num_hidden_layers):
+        layers.extend([
+            nn.Linear(num_neurons_per_hidden_layer, num_neurons_per_hidden_layer),
+            nn.ReLU(),
+        ])
+    layers.append(nn.Linear(num_neurons_per_hidden_layer, num_out))
+    if final_activation is not None:
+        layers.append(final_activation)
+    return nn.Sequential(*layers)
+
+class NormalPolicyNet(nn.Module):
+
+    """Outputs a distribution with parameters learnable by gradient descent."""
+
+    def __init__(self, input_dim, action_dim, hidden_dim):
+        super(NormalPolicyNet, self).__init__()
+        self.shared_net   = get_net(num_in=input_dim, num_out=64, final_activation=nn.ReLU(), num_neurons_per_hidden_layer=hidden_dim)
+        self.means_net    = nn.Linear(64, action_dim)
+        self.log_stds_net = nn.Linear(64, action_dim)
+
+    def forward(self, states: torch.tensor):
+
+        out = self.shared_net(states)
+        means, log_stds = self.means_net(out), self.log_stds_net(out)
+
+        # the gradient of computing log_stds first and then using torch.exp
+        # is much more well-behaved then computing stds directly using nn.Softplus()
+        # ref: https://github.com/openai/spinningup/blob/master/spinup/algos/pytorch/sac/core.py#L26
+
+        LOG_STD_MAX = 2
+        LOG_STD_MIN = -20
+        stds = torch.exp(torch.clamp(log_stds, LOG_STD_MIN, LOG_STD_MAX))
+        return Independent(Normal(loc=means, scale=stds), reinterpreted_batch_ndims=1)
+
+class QNet(nn.Module):
+
+    """Has little quirks; just a wrapper so that I don't need to call concat many times"""
+
+    def __init__(self, input_dim, action_dim, hidden_dim):
+        super(QNet, self).__init__()
+        self.net = get_net(num_in=input_dim+action_dim, num_out=1, final_activation=None, num_neurons_per_hidden_layer=hidden_dim)
+
+    def forward(self, states: torch.tensor, actions: torch.tensor):
+        return self.net(torch.cat([states, actions], dim=1))
+
+
+class SACAgent:
+
+    def __init__(self,
+                 name,
+                 reward_free,
+                 obs_shape,
+                 action_shape,
+                 device,
+                 lr,
+                 hidden_dim,
+                 gamma,
+                 tau,
+                 alpha,
+                 obs_type,
+                 target_step_interval,
+                 update_every_steps,
+                 automatic_entropy_tuning,
+                 use_tb,
+                 use_wandb,
+                 num_expl_steps,
+                 batch_size,
+                 init_critic,
+                 nstep,
+                 meta_dim = 0):
+    
+        self.obs_shape = obs_shape[0] + meta_dim
+        self.action_shape = action_shape[0]
+        self.hidden_dim = hidden_dim
+        self.device = device
+        self.lr = lr
+
+        self.reward_free = reward_free
+        self.obs_type = obs_type
+        self.use_wandb = use_wandb
+        self.use_tb = use_tb
+        
+        print(f'obs_shape: {self.obs_shape}, action_shape: {self.action_shape}, hidden_dim: {self.hidden_dim}')
+        self.Normal = NormalPolicyNet(input_dim=self.obs_shape, action_dim=self.action_shape, hidden_dim=self.hidden_dim).to(device)
+        self.Normal_optimizer = optim.Adam(self.Normal.parameters(), lr=lr)
+
+        self.Q1 = QNet(input_dim=self.obs_shape, action_dim=self.action_shape, hidden_dim=self.hidden_dim).to(device)
+        self.Q1_targ = QNet(input_dim=self.obs_shape, action_dim=self.action_shape, hidden_dim=self.hidden_dim).to(device)
+        self.Q1_targ.load_state_dict(self.Q1.state_dict())
+        self.Q1_optimizer = optim.Adam(self.Q1.parameters(), lr=lr)
+
+        self.Q2 = QNet(input_dim=self.obs_shape, action_dim=self.action_shape, hidden_dim=self.hidden_dim).to(device)
+        self.Q2_targ = QNet(input_dim=self.obs_shape, action_dim=self.action_shape, hidden_dim=self.hidden_dim).to(device)
+        self.Q2_targ.load_state_dict(self.Q2.state_dict())
+        self.Q2_optimizer = optim.Adam(self.Q2.parameters(), lr=lr)
+
+        self.gamma = gamma
+        self.alpha = alpha
+        self.polyak = 1-tau
+        
+        self.train()
+
+        print(f'Actor --> {self.Normal}')
+        print(f'Critic --> Q1: {self.Q1}, Q1_targ: {self.Q1_targ}, Q2: {self.Q2}, Q2_targ: {self.Q2_targ}')
+    
+    def train(self, training=True):
+        self.training = training
+        self.Normal.train(training)
+        self.Q1.train(training)
+        self.Q2.train(training)
+
+    def min_i_12(self, a: torch.tensor, b: torch.tensor) -> torch.tensor:
+        return torch.min(a, b)
+
+    def sample_action_and_compute_log_pi(self, state: torch.tensor, use_reparametrization_trick: bool) -> tuple:
+        mu_given_s = self.Normal(state)
+        u = mu_given_s.rsample() if use_reparametrization_trick else mu_given_s.sample()
+        a = torch.tanh(u)
+        log_pi_a_given_s = mu_given_s.log_prob(u) - (2 * (np.log(2) - u - F.softplus(-2 * u))).sum(dim=1)
+        return a, log_pi_a_given_s
+
+    def clip_gradient(self, net: nn.Module) -> None:
+        for param in net.parameters():
+            param.grad.data.clamp_(-1, 1)
+
+    def polyak_update(self, old_net: nn.Module, new_net: nn.Module) -> None:
+        for old_param, new_param in zip(old_net.parameters(), new_net.parameters()):
+            old_param.data.copy_(old_param.data * self.polyak + new_param.data * (1 - self.polyak))
+
+    def update_critic(self, obs, action, reward, discount, next_obs, step):
+        
+        metrics = dict()
+
+        with torch.no_grad():
+            next_action, log_pi_next_action_given_next_state = self.sample_action_and_compute_log_pi(next_obs, use_reparametrization_trick=False)
+            targets = reward + self.gamma * (1-discount) * (self.min_i_12(self.Q1_targ(next_obs, next_action), self.Q2_targ(next_obs, next_action)) - self.alpha * log_pi_next_action_given_next_state)
+
+        Q1_predictions = self.Q1(obs, action)
+        Q1_loss = torch.mean((Q1_predictions - targets) ** 2)
+
+        self.Q1_optimizer.zero_grad()
+        Q1_loss.backward()
+        self.clip_gradient(net=self.Q1)
+        self.Q1_optimizer.step()
+
+        Q2_predictions = self.Q2(obs, action)
+        Q2_loss = torch.mean((Q2_predictions - targets) ** 2)
+
+        self.Q2_optimizer.zero_grad()
+        Q2_loss.backward()
+        self.clip_gradient(net=self.Q2)
+        self.Q2_optimizer.step()
+
+        metrics['train/Q1_loss'] = Q1_loss.item()
+        metrics['train/Q2_loss'] = Q2_loss.item()
+
+        return metrics
+
+    def update_actor(self, obs, step):
+        
+        metrics = dict()
+
+        for param in self.Q1.parameters():
+            param.requires_grad = False
+        for param in self.Q2.parameters():
+            param.requires_grad = False
+        
+        a, log_pi_a_given_s = self.sample_action_and_compute_log_pi(obs, use_reparametrization_trick=True)
+        policy_loss = -torch.mean(self.min_i_12(self.Q1(obs, a), self.Q2(obs, a)) - self.alpha * log_pi_a_given_s)
+
+        self.Normal_optimizer.zero_grad()
+        policy_loss.backward()
+        self.clip_gradient(net=self.Normal)
+        self.Normal_optimizer.step()
+
+        for param in self.Q1.parameters():
+            param.requires_grad = True
+        for param in self.Q2.parameters():
+            param.requires_grad = True
+        return metrics
+
+    def update(self, b: Batch, step):
+
+        metrics = dict()
+
+        obs, action, reward, next_obs, discount = utils.to_torch((b.s, b.a, b.r, b.ns, b.d), self.device)
+
+        metrics.update(
+            self.update_critic(obs, action, reward, discount, next_obs, step)
+        )
+
+        metrics.update(
+            self.update_actor(obs, step)
+        )
+
+        # ========================================
+        # Step 15: update target networks
+        # ========================================
+
+        with torch.no_grad():
+            self.polyak_update(old_net=self.Q1_targ, new_net=self.Q1)
+            self.polyak_update(old_net=self.Q2_targ, new_net=self.Q2)
+
+        return metrics
+
+    def act(self, obs: np.array, step, eval_mode) -> np.array:
+        # state = torch.tensor(state).unsqueeze(0).float()
+        obs = torch.as_tensor(obs, device=self.device).unsqueeze(0).float()
+        action, _ = self.sample_action_and_compute_log_pi(obs, use_reparametrization_trick=False)
+        return action.cpu().numpy()[0]  # no need to detach first because we are not using the reparametrization trick
---
+++ train_sac.py
@@ -0,0 +1,220 @@
+import hydra
+import numpy as np
+import torch
+import wandb
+from omegaconf import OmegaConf
+from dm_env import specs
+from pathlib import Path
+
+from utils.env_constructor import make, ENV_TYPES
+import utils.utils as utils
+from utils.logger import Logger
+from utils.replay_buffer import ReplayBuffer, Transition
+from utils.video import TrainVideoRecorder, VideoRecorder
+
+torch.backends.cudnn.benchmark = True
+from libraries.dmc.dmc_tasks import PRIMAL_TASKS
+
+from tqdm import tqdm
+import libraries.safe.simple_point_bot as spb
+
+def make_agent(obs_type, obs_spec, action_spec, num_expl_steps, cfg):
+    cfg.obs_type = obs_type
+    cfg.obs_shape = obs_spec.shape
+    cfg.action_shape = action_spec.shape
+    cfg.num_expl_steps = num_expl_steps
+    return hydra.utils.instantiate(cfg)
+
+class Workspace:
+    def __init__(self, cfg):
+        self.work_dir = Path.cwd()
+        self.log_dir = cfg.log_dir
+        self.cfg = cfg
+        utils.set_seed_everywhere(cfg.seed)
+        self.device = torch.device(cfg.device)
+        # create logger
+        if cfg.use_wandb:
+            exp_name = '_'.join([
+                cfg.experiment, cfg.agent.name, cfg.domain, cfg.obs_type,
+                str(cfg.seed)
+            ])
+            wandb.init(project="urls", group=cfg.agent.name, name=exp_name, config=OmegaConf.to_container(cfg, resolve=True))
+
+        self.logger = Logger(self.work_dir,
+                             use_tb=cfg.use_tb,
+                             use_wandb=cfg.use_wandb)
+        self.env_type = ENV_TYPES[self.cfg.domain]
+
+        # create envs
+        if self.env_type in ('gym', 'safe'):
+            task = self.cfg.domain
+        else:
+            task = PRIMAL_TASKS[self.cfg.domain]
+
+        self.train_env = make(task, cfg.obs_type, cfg.frame_stack,
+                              cfg.action_repeat, cfg.seed, cfg.random_start)
+        self.eval_env = make(task, cfg.obs_type, cfg.frame_stack,
+                             cfg.action_repeat, cfg.seed, cfg.random_start)
+        print(f"obs_type: {cfg.obs_type}")
+        print(f"obs_spec: {self.train_env.observation_spec()}")
+        print(f"action_spec: {self.train_env.action_spec()}")
+        print(f"num_expl_steps: {cfg.num_seed_frames // cfg.action_repeat}")
+        print(f"agent: {cfg.agent}")
+
+         # create agent
+        self.agent = make_agent(cfg.obs_type,
+                                self.train_env.observation_spec(),
+                                self.train_env.action_spec(),
+                                cfg.num_seed_frames // cfg.action_repeat,
+                                cfg.agent)
+
+        self.goal = (150, 75)
+
+        self.replay_buffer = ReplayBuffer(capacity=int(cfg.replay_buffer_size))
+
+        # create video recorders
+        self.video_recorder = VideoRecorder(
+            self.work_dir if cfg.save_video else None,
+            camera_id=0 if 'quadruped' not in self.cfg.domain else 2,
+            use_wandb=self.cfg.use_wandb)
+
+        self._timer = utils.Timer()
+        self._global_step = 0
+        self._global_episode = 0
+
+    @property
+    def global_step(self):
+        return self._global_step
+
+    @property
+    def global_episode(self):
+        return self._global_episode
+
+    @property
+    def global_frame(self):
+        return self.global_step * self.cfg.action_repeat
+
+    def get_goal_p_star(self, agent_pos):
+        x_dist = agent_pos[0] - self.goal[0]
+        y_dist = agent_pos[1] - self.goal[1]
+        # x_dist = x_dist.cpu().detach().numpy()
+        # y_dist = y_dist.cpu().detach().numpy()
+        dist = np.linalg.norm((x_dist, y_dist), axis=0)
+        # def _prior_distro(dist):
+        #     if dist > 1.0:
+        #         p_star = -np.log(dist)
+        #     else:
+        #         p_star = 1.0
+        #     return p_star
+        # p_star = _prior_distro(dist)
+        p_star = -0.01 * dist
+        # p_star = np.array(list(map(_prior_distro, dist)), dtype=np.float32)
+        return p_star
+
+    def eval(self):
+        step, episode, total_reward = 0, 0, 0
+        eval_until_episode = utils.Until(self.cfg.num_eval_episodes)
+        while eval_until_episode(episode):
+            time_step = self.eval_env.reset()
+            self.video_recorder.init(self.eval_env, enabled=(episode==0))
+            while not time_step.last():
+                with torch.no_grad(), utils.eval_mode(self.agent):
+                    action = self.agent.act(time_step.observation, self.global_step, eval_mode=True)
+                    time_step = self.eval_env.step(action)
+                    self.video_recorder.record(self.eval_env)
+                    if self.cfg.domain == 'SimplePointBot':
+                        reward = self.get_goal_p_star(time_step.observation)
+                    else:
+                        reward = time_step.reward
+                    total_reward += reward
+                    step += 1
+            
+            episode += 1
+            self.video_recorder.save(f"{self.global_frame}.mp4")
+            
+            with self.logger.log_and_dump_ctx(self.global_frame, ty='eval') as log:
+                log('episode_reward', total_reward / episode)
+                log('episode_length', step * self.cfg.action_repeat / episode)
+                log('episode', self.global_episode)
+                log('step', self.global_step)
+
+            step, total_reward = 0, 0
+
+    
+    def train(self):
+
+        # predicates
+        train_until_step = utils.Until(self.cfg.num_train_frames, self.cfg.action_repeat)
+        eval_every_step = utils.Every(self.cfg.eval_every_frames, self.cfg.action_repeat)
+
+        episode_step, episode_reward = 0, 0
+        time_step = self.train_env.reset()
+        obs = time_step.observation
+        done = False
+        metrics = None
+        while train_until_step(self.global_step):
+
+            if time_step.last():
+                self._global_step += 1
+
+                if metrics is not None:
+                    elapsed_time, total_time = self._timer.reset()
+                    episode_frame = episode_step * self.cfg.action_repeat
+                    fps = episode_frame / elapsed_time
+                    with self.logger.log_and_dump_ctx(self.global_frame, ty='train') as log:
+                        log('fps', fps)
+                        log('total_time', total_time)
+                        log('episode_reward', episode_reward)
+                        log('episode_length', episode_frame)
+                        log('episode', self.global_episode)
+                        log('buffer_size', len(self.replay_buffer.memory))
+                        log('step', self.global_step)
+
+                # reset env
+                time_step = self.train_env.reset()
+                episode_step, episode_reward = 0, 0
+
+            # try to evaluate
+            if eval_every_step(self.global_step):
+                self.eval()
+
+            # sample action
+            with torch.no_grad(), utils.eval_mode(self.agent):
+                action = self.agent.act(time_step.observation, self.global_step, eval_mode=False)
+
+            # try to update the agent
+            if self.replay_buffer.ready_for(self.cfg.batch_size):
+                metrics = self.agent.update(self.replay_buffer.sample(self.cfg.batch_size), self.global_step)
+                self.logger.log_metrics(metrics, self.global_frame, ty='train')
+
+            # take env step
+            time_step = self.train_env.step(action)
+
+            if time_step.last():
+                done = True
+            else:
+                done = False
+            if self.cfg.domain == "SimplePointBot":
+                reward = self.get_goal_p_star(time_step.observation)
+            else:
+                reward = time_step.reward
+            next_obs = time_step.observation
+            # store to replay buffer
+            transition = Transition(obs, action, reward, next_obs, done)
+            self.replay_buffer.push(transition)
+            obs = next_obs
+            episode_step += 1
+            episode_reward += reward
+            self._global_step += 1
+
+    def run(self):
+        self.train()
+
+    @hydra.main(version_base="1.2", config_path="configs/.", config_name="sac_train")
+    def main(cfg):
+        from train_sac import Workspace as W
+        workspace = W(cfg)
+        workspace.run()
+
+    if __name__=="__main__":
+        main()
