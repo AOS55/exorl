fd1fe484dbed24690a08b8f9ade12eac95510a33
quessy <aq15777@bristol.ac.uk>
2023-01-16 11:45:33 +0000

    change to seed

--- gcc_run.sh
+++ gcc_run.sh
@@ -4,5 +4,5 @@ export DISPLAY=:0
 export HYDRA_FULL_ERROR=1
 
 for seed in 1 2 3 4 5 do
-    python train_sac.py domain=SimpleVelocityBot alpha=$alpha &
+    python pretrain.py agent=smm domain=SimpleVelocityBot seed=$seed &
 done
